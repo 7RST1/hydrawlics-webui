15e58c7d8831fb1bb97fdee6a8a0832080d03adb
Watello <141849726+Watello@users.noreply.github.com>
2025-10-17 14:40:37 +0200

    Fixed website with new algorithm for image shown

--- ContourDetection.py
+++ ContourDetection.py
@@ -1,24 +1,27 @@
 import cv2
 import numpy as np
-from CannyEdge import Canny_detector, img
+from CannyEdge import Canny_detector
 
+def get_contours(img):
 
-edges = Canny_detector(img)
-cv2.waitKey(0)
-edges = edges.astype(np.uint8)
+    edges = Canny_detector(img)
+    edges = edges.astype(np.uint8)
 
-contours, hierarchy = cv2.findContours(edges,
-                      cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contours, _ = cv2.findContours(
+        edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
+    )
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
+
+    return contours
 
 #print("Number of Contours Found = " + str(len(contours)))
 
-img_with_contours = img.copy()
+# img_with_contours = img.copy()
+# result_contours = get_contours(img)
 
-cv2.drawContours(img_with_contours, contours, -1, (0, 255, 0), 2)
-cv2.imshow('Contours', img_with_contours)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.drawContours(img_with_contours, result_contours, -1, (0, 255, 0), 2)
+# cv2.imshow('Contours', img_with_contours)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
